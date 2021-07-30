14681ee914226f467f4b41fdba34ae890e5c12fa
Raphaël <groupes.raphael@gmail.com>
2021-07-30 12:06:28 +0200

    Cleanup and minified p5js

--- html-p5js-basic-template/sketch.js
+++ html-p5js-basic-template/sketch.js
@@ -1,9 +1,22 @@
-// Activate when publishing
-p5.disableFriendlyErrors = true;
+// BASIC TEMPLATE
+
+// p5.js template for minting on Hic et Nunc
+// Made for Processing's 20th anniversary Fundraiser
+// By Raphaël de Courville (@sableraph)
+
+// hicetnunc code is adapted from https://github.com/hicetnunc2000/hicetnunc/tree/main/templates/html-p5js-template
+// More hicetnunc html templates (three.js, glsl, etc) can be found at
+// https://github.com/hicetnunc2000/hicetnunc/tree/main/templates
 
 // **************************
 // *       PARAMETERS       *
 // **************************
+
+// Activate when publishing
+p5.disableFriendlyErrors = false;
+
+// The title of your piece goes here (not visible on hicetnunc)
+document.title = "My beautiful p5.js sketch";
 
 // **************************
 // *    GLOBAL VARIABLES    *
@@ -49,3 +62,34 @@ function keyPressed() {
     console.log("C was pressed");
   }
 }
+
+// **************************
+// *     ACCESSIBILITY      *
+// **************************
+
+// We keep focus outlines active for keyboard users (see css)
+function detectKeyboardUser(e) {
+  // If user presses tab
+  if (e.keyCode === 9) {
+    console.log("Keyboard user detected");
+    document.body.classList.add("keyboardNav");
+    window.removeEventListener("keydown", detectKeyboardUser);
+  }
+}
+window.addEventListener("keydown", detectKeyboardUser);
+
+// **************************
+// * HIC ET NUNC VARIABLES  *
+// **************************
+
+// If you want to create OBJKT's with different seeds,
+// you can access the creator and viewer wallet ids.
+// This values will only be injected once the piece has been minted
+// they will not work locally.
+const creator = new URLSearchParams(window.location.search).get("creator");
+const viewer = new URLSearchParams(window.location.search).get("viewer");
+// NOTE: if the user is viewing the page on hicetnunc while unsynced,
+// the viewer variable will return a string of value "false" (NOT a boolean)
+
+console.log("NFT created by", creator); // null if local
+console.log("NFT viewed by", viewer); // null if local
