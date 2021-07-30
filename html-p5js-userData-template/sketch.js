44c2e8f075f7649df08a5c6224488617fb3d88c4
Raphaël <groupes.raphael@gmail.com>
2021-07-30 17:33:53 +0200

    Added user data template

---
+++ html-p5js-userData-template/sketch.js
@@ -0,0 +1,109 @@
+// USER DATA TEMPLATE
+
+// Make a piece that looks different based on who is looking at it
+
+// p5.js template for minting on Hic et Nunc
+// Made for Processing's 20th anniversary Fundraiser
+// By Raphaël de Courville (@sableraph)
+
+// hicetnunc code is adapted from https://github.com/hicetnunc2000/hicetnunc/tree/main/templates/html-p5js-template
+// More hicetnunc html templates (three.js, glsl, etc) can be found at
+// https://github.com/hicetnunc2000/hicetnunc/tree/main/templates
+
+// **************************
+// * HIC ET NUNC USER DATA  *
+// **************************
+
+// If you want to create OBJKT's with different seeds,
+// you can access the creator and viewer wallet ids.
+// This values will only be injected once the piece has been minted
+// they will not work locally.
+const creator = new URLSearchParams(window.location.search).get("creator");
+const viewer = new URLSearchParams(window.location.search).get("viewer");
+// NOTE: if the user is viewing the page on hicetnunc while unsynced,
+// the viewer variable will return a string of value "false" (NOT a boolean)
+
+console.log("NFT created by", creator); // null if local
+console.log("NFT viewed by", viewer); // null if local
+
+const DUMMY = "tz1hfuVWgcJ89ZE75ut9Qroi3y7GFJL5Lf2K"; // simulate a synced viewer
+const UNSYNCED = "false"; // simulate an unsynced user
+
+// Try with DUMMY or UNSYNCED only for debugging
+let viewerData = viewer;
+
+// Check if we have a user
+let viewerWasFound = viewer && !viewer.includes("false");
+
+// **************************
+// *       PARAMETERS       *
+// **************************
+
+// Use a random seed by default
+let defaultSeed = Math.floor(Math.random(999999999));
+// let defaultSeed = 123456789;
+
+// Activate when publishing
+p5.disableFriendlyErrors = false;
+
+// The title of your piece goes here (not visible on hicetnunc)
+document.title = "My beautiful p5.js sketch";
+
+// **************************
+// *        PRELOAD         *
+// **************************
+
+function preload() {}
+
+// **************************
+// *          SETUP         *
+// **************************
+
+function setup() {
+  let viewerSeed;
+  if (viewerWasFound) {
+    viewerSeed = getHash(viewerData);
+  } else {
+    viewerSeed = defaultSeed;
+  }
+
+  // Get the same random and noise value every time for a give (synced) user
+  noiseSeed(viewerSeed);
+  randomSeed(viewerSeed);
+
+  createCanvas(windowWidth, windowHeight);
+}
+
+// **************************
+// *          DRAW          *
+// **************************
+
+function draw() {
+  background(0);
+  translate(width / 2, height / 2);
+  circle(0, 0, sin(frameCount * 0.01) * min(width, height));
+}
+
+// **************************
+// *        RESIZED         *
+// **************************
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// **************************
+// *         UTILS          *
+// **************************
+
+function getHash(string) {
+  if (string) {
+    let nameHash = string.split("").reduce((a, b) => {
+      a = (a << 5) - a + b.charCodeAt(0);
+      return a & a;
+    }, 0);
+    return Math.abs(nameHash);
+  } else {
+    return null;
+  }
+}
